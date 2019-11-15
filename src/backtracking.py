18e9ae7be574adcf7310c8e5e4f784ce03a8c548
Richa <richa13sharmaa@gmail.com>
2019-11-16 00:07:39 +0530

    backtracking implementation added

---
+++ src/backtracking.py
@@ -0,0 +1,108 @@
+def clean(testcase):
+    temp = testcase.replace('.','0')
+    board = []
+    for i in range(9):
+        row = []
+        for j in range(9):
+            row.append(temp[(9*i)+j])
+        board.append(row)
+    new_list = [[int(x) for x in lst] for lst in board]
+    return new_list
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+def find_empty_location(arr,l): 
+    for row in range(9): 
+        for col in range(9): 
+            if(arr[row][col]==0): 
+                l[0]=row 
+                l[1]=col 
+                return True
+    return False
+  
+# Returns a boolean which indicates whether any assigned entry 
+# in the specified row matches the given number. 
+def used_in_row(arr,row,num): 
+    for i in range(9): 
+        if(arr[row][i] == num): 
+            return True
+    return False
+  
+# Returns a boolean which indicates whether any assigned entry 
+# in the specified column matches the given number. 
+def used_in_col(arr,col,num): 
+    for i in range(9): 
+        if(arr[i][col] == num): 
+            return True
+    return False
+  
+# Returns a boolean which indicates whether any assigned entry 
+# within the specified 3x3 box matches the given number 
+def used_in_box(arr,row,col,num): 
+    for i in range(3): 
+        for j in range(3): 
+            if(arr[i+row][j+col] == num): 
+                return True
+    return False
+  
+def check_location_is_safe(arr,row,col,num): 
+      
+    # Check if 'num' is not already placed in current row, 
+    # current column and current 3x3 box 
+    return not used_in_row(arr,row,num) and not used_in_col(arr,col,num) and not used_in_box(arr,row - row%3,col - col%3,num) 
+  
+# Takes a partially filled-in grid and attempts to assign values to 
+# all unassigned locations in such a way to meet the requirements 
+# for Sudoku solution (non-duplication across rows, columns, and boxes) 
+def solve_sudoku(arr): 
+           
+    l=[0,0] 
+      
+    # If there is no unassigned location, we are done     
+    if(not find_empty_location(arr,l)): 
+        return True
+      
+    # Assigning list values to row and col that we got from the above Function  
+    row=l[0] 
+    col=l[1] 
+      
+    # consider digits 1 to 9 
+    for num in range(1,10): 
+          
+        # if looks promising 
+        if(check_location_is_safe(arr,row,col,num)): 
+              
+            # make tentative assignment 
+            arr[row][col]=num 
+  
+            # return, if success, ya! 
+            if(solve_sudoku(arr)): 
+                return True
+  
+            # failure, unmake & try again 
+            arr[row][col] = 0
+              
+    # this triggers backtracking         
+    return False  
+
+
+if __name__=="__main__": 
+    testcase = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+    board = clean(testcase)
+    print_board(board)
+    solve_sudoku(board)
+    print("Solving...")
+    print("========================")
+    print_board(board) 
