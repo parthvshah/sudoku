75ef63ce88a7c01a6552525ac23ef0b90e3f914b
Richa <richa13sharmaa@gmail.com>
2019-11-17 22:40:58 +0530

    Stats added plus organisation

--- src/bench.py
+++ src/bench.py
@@ -5,25 +5,27 @@ import time
 
 if __name__ == "__main__":
     f1 = open("../data/easy.txt", "r")
-    # f2 = open("../data/hard.txt", "r")
+    f2 = open("../data/hard.txt", "r")
+    f3 = open("../data/extreme.txt", "r")
 
     easy = f1.readlines()
-    # hard = f2.readlines()
+    hard = f2.readlines()
+    extreme = f3.readlines()
 
-    # print("Benching with easy test cases for [csp], [gen], [bt]:")
-    # print("[Heurestics]")
+    print("Benching with easy test cases for [csp], [gen], [bt]:")
+    print("[Heurestics]")
     
-    # csp_time = 0.0
-    # for t in easy:
-    #     csp_time = 0.0 
-    #     start = time.time()
-    #     csp.main(t)
-    #     end = time.time()
-    #     csp_time += (end-start)
-    #     print(csp_time)
+    csp_time = 0.0
+    for t in easy:
+        csp_time = 0.0 
+        start = time.time()
+        csp.main(t)
+        end = time.time()
+        csp_time += (end-start)
+        # print(csp_time)
 
-    # print("Total time [csp]:", csp_time)
-    # print("Average time [csp]:", csp_time/len(easy))
+    print("Total time [csp]:", csp_time)
+    print("Average time [csp]:", csp_time/len(easy))
 
     # gen_time = 0.0
     # for t in easy:
@@ -35,44 +37,58 @@ if __name__ == "__main__":
     # print("Total time [gen]:", gen_time)
     # print("Average time [gen]:", gen_time/len(easy))
 
-    # bt_time = 0.0
+    bt_time = 0.0
     for t in easy:
         bt_time =0.0
         start = time.time()        
         bt.main(t)
         end = time.time()
         bt_time += (end-start)
-        print(bt_time)
+        # print(bt_time)
 
-    # print("Total time [bt]:", bt_time)
-    # print("Average time [bt]:", bt_time/len(easy))
+    print("Total time [bt]:", bt_time)
+    print("Average time [bt]:", bt_time/len(easy))
 
-    # print("Benching with hard test cases for [csp], [bt]:")
-    # print("[Heurestics]")
-    # csp_time = 0.0
+    print("Benching with hard test cases for [csp], [bt]:")
+    print("[Heurestics]")
+    csp_time = 0.0
     # sum =0.0
-    # for t in hard:
-    #     csp_time =0.0
-    #     start = time.time()
-    #     csp.main(t)
-    #     end = time.time()
-    #     csp_time += (end-start)
-    #     sum += csp_time
-    #     print(csp_time)
+    for t in hard:
+        # csp_time =0.0
+        start = time.time()
+        csp.main(t)
+        end = time.time()
+        csp_time += (end-start)
+        # sum += csp_time
+        # print(csp_time)
 
-    # print("Total time [csp]:", sum)
-    # print("Average time [csp]:", sum/len(hard))
+    print("Total time [csp]:", csp_time)
+    print("Average time [csp]:", csp_time/len(hard))
 
-    # bt_time = 0.0
+    bt_time = 0.0
     # sum = 0.0
-    # for t in hard:
-    #     bt_time = 0.0
-    #     start = time.time() 
-    #     bt.main(t)
-    #     end = time.time()
-    #     bt_time += (end-start)
+    for t in hard:
+    #   bt_time = 0.0
+        start = time.time() 
+        bt.main(t)
+        end = time.time()
+        bt_time += (end-start)
     #     sum += bt_time
     #     print(bt_time)
 
-    # print("Total time [bt]:", sum)
-    # print("Average time [bt]:", sum/len(hard))
+    print("Total time [bt]:", bt_time)
+    print("Average time [bt]:", bt_time/len(hard))
+
+    
+    print("Benching with extreme test cases for [csp]:")
+    # csp_time = 0.0
+    # sum =0.0
+    for t in extreme:
+        csp_time =0.0
+        start = time.time()
+        csp.main(t)
+        end = time.time()
+        csp_time += (end-start)
+        print(csp_time)
+
+    
--- src/stats.py
+++ src/stats.py
@@ -1,10 +1,19 @@
 import statistics
 
-f_csp_h = open("../src/csp_h.txt", "r")
-f_bt_h = open("../src/bt_h.txt", "r")
+f_csp_h = open("../data/csp_h.txt", "r")
+f_bt_h = open("../data/bt_h.txt", "r")
+
+f_csp_e = open("../data/csp_e.txt", "r")
+f_bt_e = open("../data/bt_e.txt", "r")
+
+f_csp_ex = open("../data/csp_ex.txt", "r")
+
 
 csp = f_csp_h.readlines()
 bt = f_bt_h.readlines()
+csp_e = f_csp_e.readlines()
+bt_e = f_bt_e.readlines()
+csp_ex = f_csp_ex.readlines()
 
 #csp hard testcases
 csp_list_h = []
@@ -14,10 +23,55 @@ csp_list_h = [i.replace('\n','') for i in csp_list_h]
 csp_list_h = [float(i) for i in csp_list_h]
 
 mean1 = statistics.mean(csp_list_h)
-print(mean1)
+var1 = statistics.variance(csp_list_h)
+print ("\nVariance of csp hard test cases is :", var1)
+print("Mean of csp hard test cases is: ",mean1)
 #bt hard cases
-# bt_list_h= []
-# for t in bt:
-#     bt_list_h.append(t)
-# bt_list_h = [i.replace('\n','') for i in bt_list_h]
-# bt_list_h = [float(i) for i in bt_list_h]
+bt_list_h= []
+for t in bt:
+    bt_list_h.append(t)
+bt_list_h = [i.replace('\n','') for i in bt_list_h]
+bt_list_h = [float(i) for i in bt_list_h]
+
+mean2 = statistics.mean(bt_list_h)
+var2= statistics.variance(bt_list_h)
+print ("\nVariance of bt hard test cases is :", var2)
+print("Mean of bt hard test cases is: ",mean2)
+
+
+#csp easy testcases
+csp_list_e = []
+for t in csp_e:
+    csp_list_e.append(t)
+csp_list_e = [i.replace('\n','') for i in csp_list_e]
+csp_list_e = [float(i) for i in csp_list_e]
+
+mean3 = statistics.mean(csp_list_e)
+var3 = statistics.variance(csp_list_e)
+print ("\nVariance of csp easy test cases is :", var3)
+print("Mean of csp hard easy cases is: ",mean3)
+
+#bt easy cases
+bt_list_e= []
+for t in bt_e:
+    bt_list_e.append(t)
+bt_list_e = [i.replace('\n','') for i in bt_list_e]
+bt_list_e = [float(i) for i in bt_list_e]
+print("\n")
+mean4 = statistics.mean(bt_list_e)
+var4= statistics.variance(bt_list_e)
+print ("\nVariance of bt easy test cases is :", var4)
+print("Mean of bt easy test cases is: ",mean4)
+
+
+#csp extreme testcases
+csp_list_ex = []
+for t in csp_ex:
+    csp_list_ex.append(t)
+csp_list_ex = [i.replace('\n','') for i in csp_list_ex]
+csp_list_ex = [float(i) for i in csp_list_ex]
+
+mean5 = statistics.mean(csp_list_ex)
+var5 = statistics.variance(csp_list_ex)
+print ("\nVariance of csp extreme test cases is :", var5)
+print("Mean of csp extreme test cases is: ",mean5)
